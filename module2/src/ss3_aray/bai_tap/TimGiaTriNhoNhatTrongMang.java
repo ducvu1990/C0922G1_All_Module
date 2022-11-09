2ea511a913c21ca1d5124c5771b85cd3c966a5f3
Đức Vũ Bùi <cubinqtvn15@gmail.com>
2022-11-09 11:18:00 +0700

    up

--- module2/src/ss3_aray/bai_tap/TimGiaTriNhoNhatTrongMang.java
+++ module2/src/ss3_aray/bai_tap/TimGiaTriNhoNhatTrongMang.java
@@ -6,14 +6,14 @@ public class TimGiaTriNhoNhatTrongMang {
     public static void main(String[] args) {
         System.out.println("Nhập độ rộng của mảng");
         Scanner sc = new Scanner(System.in);
-        int arrayLength = Integer.parseInt(sc.nextLine());
-        int[] array = new int[arrayLength];
-        for (int i = 0; i < arrayLength; i++) {
+        int size = Integer.parseInt(sc.nextLine());
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
                 System.out.print("Nhập giá trị array[" + i + "]: ");
                 array[i] = Integer.parseInt(sc.nextLine());
         }
         int min = array[0];
-        for (int i = 0; i < arrayLength; i++) {
+        for (int i = 0; i < size; i++) {
                 if (min >  array[i]){
                     min = array[i];
             }
