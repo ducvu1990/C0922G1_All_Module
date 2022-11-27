97397b78710ed583966e9869751085d1500ae6a7
Đức Vũ Bùi <cubinqtvn15@gmail.com>
2022-11-27 14:45:16 +0700

    up

--- module2/src/ss15_exception_debug/bai_tap/IllegalTriangleException.java
+++ module2/src/ss15_exception_debug/bai_tap/IllegalTriangleException.java
@@ -1,9 +1,15 @@
 package ss15_exception_debug.bai_tap;
 
 public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException() {
+    }
+
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
 
     @Override
     public String getMessage() {
-        return "Sai điều kiện của tam giác";
+        return super.getMessage();
     }
 }
--- module2/src/ss15_exception_debug/bai_tap/Triangle.java
+++ module2/src/ss15_exception_debug/bai_tap/Triangle.java
@@ -23,7 +23,7 @@ public class Triangle {
                 break;
 
             } catch (IllegalTriangleException e) {
-                System.out.println("Sai điều kiện tam giác");
+                System.err.println(e.getMessage());
             }catch (NumberFormatException e){
                 System.err.println(e.getMessage());
             }
@@ -31,13 +31,13 @@ public class Triangle {
     }
     public static void checkTriangle (double canhA, double canhB, double canhC) throws IllegalTriangleException{
         if ((canhA + canhB) <= canhC || (canhA + canhC) <= canhB || (canhB + canhC) <= canhA){
-           throw new IllegalTriangleException();
+           throw new IllegalTriangleException("Tổng 2 cạnh phải lớn hơn cạnh còn lại của tam giác!");
         }
     }
 
     public static void checkTriangleCanh (double canh) throws IllegalTriangleException{
         if (canh <= 0){
-            throw new IllegalTriangleException();
+            throw new IllegalTriangleException("Cạnh tam giác phải là một số, và số đó phải là số dương!");
         }
     }
 }
